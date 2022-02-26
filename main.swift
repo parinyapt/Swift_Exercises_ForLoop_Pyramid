50054a4f8c4c222464fb729a8843a83783e56757
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2022-02-26 15:51:49 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+var temp:String = ""
+
+print("Enter Pyramid Height (Number) : ", terminator: "")
+let height:Int = Int(readLine()!)!
+
+for row in 1...height {
+  for _ in 1...((height+1)-row) {
+    temp += " "
+  }
+  for normalNumber in 1...row {
+    temp += String(normalNumber)
+  }
+  for reverseNumber in (0...(row-1)).reversed() {
+    temp += reverseNumber == 0 ? "" : String(reverseNumber)
+  }
+  temp += "\n"
+}
+print(temp)
